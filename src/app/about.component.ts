16eeda0ee9eaae353da0317f9bd9d7274f2ab64b
teachingty <bv9t-yn@live.jp>
2021-03-17 19:42:05 +0900

    2 files modified

--- src/app/about.component.ts
+++ src/app/about.component.ts
@@ -1,14 +1,8 @@
-import { templateJitUrl } from "@angular/compiler";
-import { Template } from "@angular/compiler/src/render3/r3_ast";
-import {Component} from "@angular/core";
+import { Component } from "@angular/core";
 
-@Component({
-  selector:'about',
-  templateUrl:'./about.component.html',
-  styleUrls:['./about.component.css']
+@AboutComponent({
+  selector: "about",
+  templateUrl: "./about.component.html",
+  styleUrls: ["./about.component.css"]
 })
-
-// export class testComponent{
-
-// }
-
+export class AboutComponent {}
