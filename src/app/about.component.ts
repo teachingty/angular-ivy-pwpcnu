8c18330cc48503397ec60e2ea0242abc067d201c
teachingty <bv9t-yn@live.jp>
2021-03-17 19:15:03 +0900

    1 file modified, 2 added

---
+++ src/app/about.component.ts
@@ -0,0 +1,14 @@
+import { templateJitUrl } from "@angular/compiler";
+import { Template } from "@angular/compiler/src/render3/r3_ast";
+import {Component} from "@angular/core";
+
+@Component({
+  selector:'about',
+  templateUrl:'./about.component.html',
+  styleUrls:['./about.component.css']
+})
+
+// export class testComponent{
+
+// }
+
