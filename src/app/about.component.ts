ae9b01f42c168576669c34a46805e4871f7210ea
teachingty <bv9t-yn@live.jp>
2021-03-18 01:13:13 +0900

    1 file modified

--- src/app/about.component.ts
+++ src/app/about.component.ts
@@ -3,6 +3,7 @@ import { Component } from "@angular/core";
 @Component({
   selector: "about",
   templateUrl: "./about.component.html",
+  // template: "<div>new html text at 20210317</div>",
   styleUrls: ["./about.component.css"]
 })
 export class AboutComponent {}
