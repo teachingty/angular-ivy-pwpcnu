75b108ba589383828bd118feaff84944a557e430
teachingty <bv9t-yn@live.jp>
2021-03-18 13:00:21 +0900

    1 file modified, 3 added, 3 deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { FormsModule } from "@angular/forms";
 
 import { AppComponent } from "./app.component";
 import { HelloComponent } from "./hello.component";
-import { AboutComponent } from "./about.component";
+import { AboutComponent } from "./about/about.component";
 
 @NgModule({
   imports: [BrowserModule, FormsModule],
